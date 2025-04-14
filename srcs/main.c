9b1666553e22b77323f100249b1addfae5a452a9
Colin Woon <colin.wyj@gmail.com>
2025-04-14 17:49:16 +0800

    feat: added hooks for wasd movement

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: cwoon <cwoon@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 06:57:25 by rteoh             #+#    #+#             */
-/*   Updated: 2025/04/14 17:23:53 by cwoon            ###   ########.fr       */
+/*   Updated: 2025/04/14 17:48:14 by cwoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,16 @@ typedef struct	s_vars {
 	void	*win;
 }				t_vars;
 
+int	close_window(int keycode, t_vars *vars);
 int	key_hook1(int keysym, t_vars *vars)
 {
-	printf("DEBUG: sym int is %d\n", keysym);
-	printf("DEBUG: sym hexa is %x\n", keysym);
+	// printf("DEBUG: sym INT is %d\n", keysym);
+	printf("DEBUG: sym HEXA is %x\n", keysym);
 	if (keysym == XK_Escape)
-		printf("Escape\n");
+	{
+		printf("DEBUG: Escape\n");
+		close_window(0, NULL);
+	}
 	else if (keysym == XK_Left)
 		printf("Left\n");
 	else if (keysym == XK_Right)
@@ -31,14 +35,28 @@ int	key_hook1(int keysym, t_vars *vars)
 		printf("Up\n");
 	else if (keysym == XK_Down)
 		printf("Down\n");
+	else if (keysym == XK_w)
+		printf("w\n");
+	else if (keysym == XK_a)
+		printf("a\n");
+	else if (keysym == XK_s)
+		printf("s\n");
+	else if (keysym == XK_d)
+		printf("d\n");
 	return (0);
 }
 
 int	close_window(int keycode, t_vars *vars)
 {
-	printf("hello");
+	printf("DEBUG: closing window\n");
 	exit(0);
 	// mlx_destroy_window(vars->mlx, vars->win);
+	return (0);
+}
+
+int debug_event(int keycode, t_vars *vars)
+{
+	printf("DEBUG: event\n");
 	return (0);
 }
 
