571664c6c6929257057d56d90ba27fd9f8550ac8
Colin Woon <colin.wyj@gmail.com>
2025-05-29 14:50:35 +0800

    chore: norm fix for draw_wall_texture, split parts

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: cwoon <cwoon@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 16:35:45 by cwoon             #+#    #+#             */
-/*   Updated: 2025/05/29 01:24:32 by cwoon            ###   ########.fr       */
+/*   Updated: 2025/05/29 14:49:22 by cwoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -127,6 +127,14 @@ typedef struct s_game
 	bool		is_render;
 }	t_game;
 
+typedef struct s_texture_vars
+{
+	int		x;
+	int		y;
+	double	pos;
+	double	step;
+	int		color;
+}	t_texture_vars;
 
 t_assets	*init_assets(void);
 bool		parse_map(int fd, char *line, t_game *game);
@@ -177,6 +185,7 @@ void	cleanup(t_game *game);
 void	run_raycasting(t_ray *ray, t_player *player, t_mlx *mlx, t_game *game);
 void	calculate_line_height(t_ray *ray);
 void	init_ray_dir_n_map_pos(t_game *game, int x, int *map_x, int *map_y);
+void	draw_wall_texture(t_img *texture, double wall_x, t_game *game, int *x);
 
 // draw_floor_ceiling.c
 void	draw_floor_ceiling(t_mlx *mlx, int floor_colour, int ceiling_colour);
@@ -191,10 +200,13 @@ void	calculate_point_gap(t_ray *ray);
 
 // textures.c
 int					get_texture_pixel(t_img *texture, int x, int y);
-void				draw_wall_texture(t_img *texture, double wall_x,\
- t_ray *ray, t_mlx *mlx, int *x);
-double				get_fractional_texture_position_x(t_ray *ray, t_player *player);
+double				get_fractional_texture_position_x\
+(t_ray *ray, t_player *player);
 t_wall_direction	get_wall_direction(t_ray *ray);
+void				put_texture_pixels\
+(t_texture_vars *tex, t_img *texture, t_game *game, int *x);
+void				prep_texture_vars\
+(t_texture_vars *tex, double wall_x, t_img *texture, t_ray *ray);
 
 // mlx_colour_utils.c
 int	create_trgb(int transparency, int red, int green, int blue);
